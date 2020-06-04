2224bfccbd7e1bea9c70c5a84ae5bc2ce256ef6a
nlgtuankiet <nlg.tuan.kiet@gmail.com>
2020-06-04 17:39:40 +0700

    remove kotlin android extension

--- app/src/main/java/com/sample/daggerworkmanagersample/MainActivity.kt
+++ app/src/main/java/com/sample/daggerworkmanagersample/MainActivity.kt
@@ -2,16 +2,16 @@ package com.sample.daggerworkmanagersample
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Button
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        hello_button.setOnClickListener {
+        findViewById<Button>(R.id.hello_button).setOnClickListener {
             WorkManager.getInstance().enqueue(
                 OneTimeWorkRequestBuilder<HelloWorldWorker>().build()
             )
